1aa2a45b325b7261be53ffac0f385e3cd37b7627
Martin Heide <martin.heide@faro.com>
2018-06-03 14:39:05 +0200

    Added tetrahedron renderable

--- Renderables/Renderables.js
+++ Renderables/Renderables.js
@@ -2,7 +2,9 @@
 // Cesium app:
 // Renderable
 //
-// * Based on official Cesium example
+// * Based on:
+//   * https://github.com/AnalyticalGraphicsInc/cesium/wiki/Geometry-and-Appearances
+//   * https://gist.github.com/zwcloud/182c2e2c23ad1aef4b2dd9e6df4c1ea7
 //
 var viewer, scene, canvas;
 
@@ -16,7 +18,9 @@ var ellipsoid = scene.globe.ellipsoid;
 // 48.786889, 9.0843
 var locSolitude2 = {
     destination: Cesium.Cartesian3.fromRadians
-        (0.15865324078031692, 0.8514895282737008, 778.7003110082524),
+       (0.15865324078031692, 0.8514895282737008, 778.7003110082524),
+    // destination: Cesium.Cartesian3.fromDegrees
+    //     (-100.0, 40.0)
     orientation: {
         heading: 4.699033167472921,
         pitch: -0.49804187544369105,
@@ -37,4 +41,28 @@ viewer.entities.add({
 });
 
 
-var entity;
+var modelMatrix = new Cesium.Matrix4();
+Cesium.Matrix4.multiplyByTranslation(
+    Cesium.Transforms.eastNorthUpToFixedFrame(
+        ellipsoid.cartographicToCartesian(Cesium.Cartographic.fromDegrees(8, 40))
+    ),
+    new Cesium.Cartesian3(0.0, 0.0, 200000.0),
+    modelMatrix);
+Cesium.Matrix4.multiplyByUniformScale(modelMatrix, 500000.0, modelMatrix);
+
+var instance = new Cesium.GeometryInstance({
+    geometry : new TetrahedronGeometry(),
+    modelMatrix : modelMatrix,
+    attributes : {
+        color : Cesium.ColorGeometryInstanceAttribute.fromColor(Cesium.Color.WHITE)
+    }
+});
+
+scene.primitives.add(new Cesium.Primitive({
+    asynchronous: false, // we don't provide a worker
+    geometryInstances : instance,
+    appearance : new Cesium.PerInstanceColorAppearance({
+        flat : true,
+        translucent : false
+    })
+}));
---
+++ Renderables/TetrahedronGeometry.js
@@ -0,0 +1,112 @@
+// * Based on:
+//   * https://github.com/AnalyticalGraphicsInc/cesium/wiki/Geometry-and-Appearances
+//   * https://gist.github.com/zwcloud/182c2e2c23ad1aef4b2dd9e6df4c1ea7
+
+"use strict";
+
+let Cartesian3 = Cesium.Cartesian3;
+let ComponentDatatype = Cesium.ComponentDatatype;
+let PrimitiveType = Cesium.PrimitiveType;
+let BoundingSphere = Cesium.BoundingSphere;
+let GeometryAttribute = Cesium.GeometryAttribute;
+let GeometryAttributes = Cesium.GeometryAttributes;
+let GeometryPipeline = Cesium.GeometryPipeline;
+let VertexFormat = Cesium.VertexFormat;
+let Geometry = Cesium.Geometry;
+
+var TetrahedronGeometry = function() {
+    this._workerName = 'createTetrahedronGeometry';
+};
+
+TetrahedronGeometry.createGeometry = function() {
+    
+    var negativeRootTwoOverThree = -Math.sqrt(2.0) / 3.0;
+    var negativeOneThird = -1.0 / 3.0;
+    var rootSixOverThree = Math.sqrt(6.0) / 3.0;
+    
+    var positions = new Float64Array(4 * 3 * 3);
+    // back triangle
+    positions[0] = 0.0;
+    positions[1] = 0.0;
+    positions[2] = 1.0;
+    positions[3] = 0.0;
+    positions[4] = (2.0 * Math.sqrt(2.0)) / 3.0;
+    positions[5] = negativeOneThird;
+    positions[6] = -rootSixOverThree;
+    positions[7] = negativeRootTwoOverThree;
+    positions[8] = negativeOneThird;
+    
+    // left triangle
+    positions[9] = 0.0;
+    positions[10] = 0.0;
+    positions[11] = 1.0;
+    positions[12] = -rootSixOverThree;
+    positions[13] = negativeRootTwoOverThree;
+    positions[14] = negativeOneThird;
+    positions[15] = rootSixOverThree;
+    positions[16] = negativeRootTwoOverThree;
+    positions[17] = negativeOneThird;
+    
+    // right triangle
+    positions[18] = 0.0;
+    positions[19] = 0.0;
+    positions[20] = 1.0;
+    positions[21] = rootSixOverThree;
+    positions[22] = negativeRootTwoOverThree;
+    positions[23] = negativeOneThird;
+    positions[24] = 0.0;
+    positions[25] = (2.0 * Math.sqrt(2.0)) / 3.0;
+    positions[26] = negativeOneThird;
+    
+    // bottom triangle
+    positions[27] = -rootSixOverThree;
+    positions[28] = negativeRootTwoOverThree;
+    positions[29] = negativeOneThird;
+    positions[30] = 0.0;
+    positions[31] = (2.0 * Math.sqrt(2.0)) / 3.0;
+    positions[32] = negativeOneThird;
+    positions[33] = rootSixOverThree;
+    positions[34] = negativeRootTwoOverThree;
+    positions[35] = negativeOneThird;
+    
+    var indices = new Uint16Array(4 * 3);
+    
+    // back triangle
+    indices[0] = 0;
+    indices[1] = 1;
+    indices[2] = 2;
+    
+    // left triangle
+    indices[3] = 3;
+    indices[4] = 4;
+    indices[5] = 5;
+    
+    // right triangle
+    indices[6] = 6;
+    indices[7] = 7;
+    indices[8] = 8;
+    
+    // bottom triangle
+    indices[9] = 9;
+    indices[10] = 10;
+    indices[11] = 11;
+
+    var attributes = new GeometryAttributes({
+        position : new GeometryAttribute({
+            componentDatatype : ComponentDatatype.DOUBLE,
+            componentsPerAttribute : 3,
+            values : positions
+        })
+    });
+
+    return new Geometry({
+        attributes : attributes,
+        indices : indices,
+        primitiveType : PrimitiveType.TRIANGLES,
+        boundingSphere : new BoundingSphere(new Cartesian3(0.0, 0.0, 0.0), 1.0)
+    });
+};
+
+TetrahedronGeometry.unpack = function(array, startingIndex, result) {
+    return new TetrahedronGeometry();//do nothing but return a defualt TetrahedronGeometry
+}
